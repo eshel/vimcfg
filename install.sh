2d2ebc03cb806b3636444d11530eb01eb2f0c7f3
Amir Eshel <amire@everything.me>
2014-04-30 18:15:19 +0300

    updated Vundle github repo url

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 mkdir bundle
-git clone https://github.com/gmarik/bundle.git ./bundle/vundle
+git clone https://github.com/gmarik/Vundle.vim.git ./bundle/vundle
 vim -c BundleInstall!
 
